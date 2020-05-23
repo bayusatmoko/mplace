96d8325d61b5dcaf4529bd6924976769c57f3913
dhevanthareza <dhevanmuhamad@gmail.com>
2020-05-23 14:26:41 +0700

    nyicil bagian layanan dan tentang

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,68 +1,139 @@
 <template>
-  <div class="about">
-    <v-container fluid class="top-landing" v-on:resize="onResize($event)" style="user-select:none;">
-      <v-row
-        style="margin-top: 20px; height: 210px;"
-        :justify="'space-between'"
-      >
-        <v-col cols="6" />
-        <v-col cols="6">
-          <div style="display: flex; flex-direction: column; height: 215px; justify-content: space-between;">
-            <div class="link-group" v-show="notResize">
-              <router-link class="link" to="/">Layanan</router-link>
-              <router-link class="link" to="/about">Tentang Kami</router-link>
-              <router-link class="link" to="/">Blog</router-link>
-              <router-link class="link" to="/about">Hubungi Kami</router-link>
+  <div>
+    <div class="about">
+      <v-container fluid class="top-landing" v-on:resize="onResize($event)" style="user-select:none;">
+        <v-row
+          style="margin-top: 20px; height: 210px;"
+          :justify="'space-between'"
+        >
+          <v-col cols="6" />
+          <v-col cols="6">
+            <div style="display: flex; flex-direction: column; height: 215px; justify-content: space-between;">
+              <div class="link-group" v-show="notResize">
+                <router-link class="link" to="/">Layanan</router-link>
+                <router-link class="link" to="/about">Tentang Kami</router-link>
+                <router-link class="link" to="/">Blog</router-link>
+                <router-link class="link" to="/about">Hubungi Kami</router-link>
+              </div>
+                <p class="cs-text-right text-center" v-show="notResize">Coming Soon</p>
             </div>
-              <p class="cs-text-right text-center" v-show="notResize">Coming Soon</p>
+          </v-col>
+        </v-row>
+            <v-row
+              :justify="'start'"
+              style="height: 200px; margin-left: 141px;"
+            >
+              <v-col cols="5">
+                <span class="question">Belanja Apa Hari Ini?</span>
+                <div class="pt-8">
+                  <span class="txt font-weight-bold">Gets id </span>
+                  <span class="txt">
+                  terhubung dengan banyak toko sembako, tempat makan, jasa pesan dan antar hingga toko buah dan sayur untuk memenuhi kebutuhan kamu.
+                  </span>
+                </div>
+                <p/>
+                <v-btn :loading="isLoading" class="shop-btn" color="#388659" elevation="6" @click="goHome">Belanja Yuk</v-btn>
+                <div>
+                <a>
+                  <img class="gplay" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+                    <p class="cs-text">Coming Soon</p>
+                </div>
+              </v-col>
+              <v-col cols="2" />
+              <v-col cols="4">
+                <div class="timer" style="margin-left:10px; height: 120px; width: 380px; border-radius: 15px; display:flex; justify-content: center; align-items: center;" v-show="notResize">
+                <Timer
+                  starttime="Jul 5, 2020 00:37:25"
+                  endtime="Jul 8, 2020 00:37:25"
+                  trans='{
+          "day":"Hari",
+          "hours":"Jam",
+          "minutes":"Menit",
+          "seconds":"Detik",
+          "expired":"Event has been expired.",
+          "running":"Till the end of event.",
+          "upcoming":"Till start of event.",
+          "status": {
+              "expired":"Expired",
+              "running":"Running",
+              "upcoming":"Future"
+            }}'
+                />
+                </div>
+              </v-col>
+            </v-row>
+      </v-container>
+    </div>
+    <div>
+        <div class="second">
+          <div class="display-1">
+            Layanan Kami
           </div>
-        </v-col>
-      </v-row>
-          <v-row
-            :justify="'start'"
-             style="height: 200px; margin-left: 141px;"
-          >
-            <v-col cols="5">
-              <span class="question">Belanja Apa Hari Ini?</span>
-              <div class="pt-8">
-                <span class="txt font-weight-bold">Gets id </span>
-                <span class="txt">
-                terhubung dengan banyak toko sembako, tempat makan, jasa pesan dan antar hingga toko buah dan sayur untuk memenuhi kebutuhan kamu.
-                </span>
-              </div>
-              <p/>
-              <v-btn :loading="isLoading" class="shop-btn" color="#388659" elevation="6" @click="goHome">Belanja Yuk</v-btn>
-              <div>
-              <a>
-                <img class="gplay" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
-                  <p class="cs-text">Coming Soon</p>
-              </div>
-            </v-col>
-            <v-col cols="2" />
+          <v-row justify="center">
             <v-col cols="4">
-              <div class="timer" style="margin-left:10px; height: 120px; width: 380px; border-radius: 15px; display:flex; justify-content: center; align-items: center;" v-show="notResize">
-              <Timer
-                starttime="Jul 5, 2020 00:37:25"
-                endtime="Jul 8, 2020 00:37:25"
-                trans='{
-         "day":"Hari",
-         "hours":"Jam",
-         "minutes":"Menit",
-         "seconds":"Detik",
-         "expired":"Event has been expired.",
-         "running":"Till the end of event.",
-         "upcoming":"Till start of event.",
-         "status": {
-            "expired":"Expired",
-            "running":"Running",
-            "upcoming":"Future"
-           }}'
-              />
-              </div>
+              <p>
+                Kami meng hubungkan pelanggan dengan para penjual untuk melakukan
+                transaksi kebutuhan sehari - hari melalui empat layanan unggulan
+                yaitu Gets Food, Gets Fresh, Gets Daily, dan Gets Service
+              </p>
             </v-col>
           </v-row>
-    </v-container>
-  </div>
+        </div>
+      </div>
+      <div class="third">
+        <v-row justify="center" class="mt-10">
+          <div class="card mx-5">
+            <v-col align="center">
+              <img src="img/penjual.png" />
+            </v-col>
+            <div style="text-align: center; margin-top:-35px">
+              <h2>PENJUAL</h2>
+              <p>
+                Tingkatkan transaksi hingga 2x lipat dengan aplikasi Gets id.
+                Terima pesanan secara online dan jangkau lebih banyak pelanggan
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <v-col align="center">
+              <img src="img/pembeli.png" />
+            </v-col>
+            <div style="text-align: center; margin-top:-35px">
+              <h2>PEMBELI</h2>
+              <p>
+                Nikmati kemudahan untuk belanja dengan satu aplikasi. Gets id
+                hadir untuk membantu belanja kebutuhan harian kamu dengan mudah,
+                cepat dan aman.
+              </p>
+            </div>
+          </div>
+        </v-row>
+      </div>
+      <div class="fourth">
+        <div style="text-align: center" class="display-1">Tentang Kami</div>
+        <v-row align="center">
+          <v-col xs6>
+            <img style="width:80%" src="img/storefront.png" />
+          </v-col>
+          <v-col xs6>
+            <div class="card2 my-7">
+              <p>
+                Gets id hadir untuk memudahkan pelanggan dalam memenuhi kebutuhan
+                sehari - hari dan mendorong aktivitas perekonomian pengusaha
+                melalui teknologi belanja online
+              </p>
+            </div>
+            <div class="card2">
+              <p>
+                Kami menghubungkan pelanggan dengan berbagai penjual seperti toko
+                sembako, tempat makan, jasa pesan dan antar hingga toko buah dan
+                sayur dengan mudah, cepat dan aman.
+              </p>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -191,4 +262,48 @@ export default {
     font-size: 25px;
     color: #5A5B5B;
   }
+  .second {
+  height: 400px;
+  padding: 100px 0px;
+  text-align: center;
+  color: white;
+  overflow: visible;
+  background-color: #6ac665;
+  border-bottom-left-radius: 50% 100px;
+  border-bottom-right-radius: 50% 100px;
+}
+.card {
+  width: 350px;
+  height: 408px;
+  padding: 10px 20px;
+  background: linear-gradient(
+      360deg,
+      #ffffff 0%,
+      rgba(255, 255, 255, 0) 196.71%
+    ),
+    #4aae48;
+  box-shadow: 0px 8px 16px rgba(74, 174, 72, 0.25);
+  border-radius: 15px;
+}
+.card2 {
+  width: 635px;
+  padding: 60px;
+  background: #ffffff;
+  box-shadow: 0px 8px 16px rgba(74, 174, 72, 0.25);
+  border-radius: 15px;
+}
+.card3 {
+  width: 375px;
+  height: 121px;
+
+  background: #388659;
+  box-shadow: 0px 8px 16px rgba(74, 174, 72, 0.25);
+  border-radius: 15px;
+}
+.third {
+  margin-top: -150px;
+}
+.fourth {
+  margin-top: 200px;
+}
 </style>
