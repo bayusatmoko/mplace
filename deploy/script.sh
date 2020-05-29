0a9725efaa04f2beb6b083bf3851fcf83a4bf3fe
19058221 <P@ssw0rd123>
2020-05-29 15:43:41 +0700

    fix deploy script

--- deploy/script.sh
+++ deploy/script.sh
@@ -4,6 +4,8 @@ cd ~/mplace
  git checkout master
  git pull origin master
  docker build -t mplace .
+ docker stop mplace
+ docker rm mplace
  docker run --name mplace -p 80:80 -d mplace
  curl localhost:80
  exit 0
