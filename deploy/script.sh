896dc45e339abefbbcf578334fa89844894273ba
19058221 <P@ssw0rd123>
2020-05-29 15:37:06 +0700

    add deploy script

---
+++ deploy/script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ssh -t root@68.183.184.97 <<EOF
+cd ~/mplace
+ git checkout master
+ git pull origin master
+ docker build -t mplace .
+ docker run --name mplace -p 80:80 -d mplace
+ curl localhost:80
+ exit 0
+EOF
+
+
