de4cf3bd535b99c80082ea85805b39f3a195157b
19058221 <P@ssw0rd123>
2020-05-29 15:52:04 +0700

    Fix

--- deploy/script.sh
+++ deploy/script.sh
@@ -6,7 +6,7 @@ cd ~/mplace
  docker build -t mplace .
  docker stop mplace
  docker rm mplace
- docker run --name mplace -p 80:80 -d mplace
+ docker run --name mplace -p 3000:3000 -d mplace
  curl localhost:80
  exit 0
 EOF
